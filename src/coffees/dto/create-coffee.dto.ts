3177bf03eb08523d71982ef43b937bda135800ed
A Kholaif <akh@thefoodlab.com>
2022-01-10 22:25:24 +0200

    Add DTO - Validation Rules,dto partialtype, white listing,blacklist

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,6 +1,15 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsNumber()
   readonly price: number;
+
+  @IsString({ each: true })
   readonly flavors: string[];
 }
