1ea26b3fdadea55d27fb1b8d8522a6e3196fd4e3
A Kholaif <akh@thefoodlab.com>
2022-01-10 21:41:46 +0200

    Handle exceptions

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -25,7 +25,12 @@ export class CoffeesService {
   }
 
   findOne(id: string): Coffee {
-    return this.coffees.find(coffee => coffee.id === +id);
+    const coffee = this.coffees.find(coffee => coffee.id === +id);
+    if (!coffee) {
+      // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
   } 
 
   create(coffee: Coffee): Coffee {
